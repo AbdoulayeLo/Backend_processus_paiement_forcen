cc40a7c5e86402dca450e135ac0cd999befcdfe0
unknown <loabdoulaye310100.com>
2025-01-19 13:18:38 +0000

    premier commit

---
+++ src/main/java/sn/forcen/processus_paiement_forcen_backend/ProcessusPaiementForcenBackendApplication.java
@@ -0,0 +1,13 @@
+package sn.forcen.processus_paiement_forcen_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessusPaiementForcenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessusPaiementForcenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/forcen/processus_paiement_forcen_backend/ProcessusPaiementForcenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.forcen.processus_paiement_forcen_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessusPaiementForcenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
